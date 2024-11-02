4afdbc8581f243f9b4e0898a1a1f62a5e91ff70f
MotieeAtefe <motiee1377@gmaile.com>
2024-11-02 12:03:10 +0330

    Add project files.

---
+++ Singleton.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace singleton
+{
+    internal class Singleton
+    {
+        private static Singleton singleton = null;
+        private static readonly object singletonLock = new object();
+        Singleton() { }
+        public static Singleton SingleInstance
+        {
+            get 
+            { 
+
+            }
+        }
+    }
+}
